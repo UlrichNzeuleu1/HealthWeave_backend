f02b7a38fa5739fa77650972ed58d2da997deac3
Ulrich Nzeuleu <ulrichnzeuleu@gmail.com>
2025-04-30 13:31:08 +0200

    Creation de l'interface AdresseApi et AssuranceApi. Ici nous voulons separe la classe controleur de la documentation de l'application et les diffrentes routes des endpoints.

--- src/main/java/com/example/Medical/App/controlleurs/api/AdresseApi.java
+++ src/main/java/com/example/Medical/App/controlleurs/api/AdresseApi.java
@@ -1,5 +1,39 @@
 package com.example.Medical.App.controlleurs.api;
 
 
+import com.example.Medical.App.dto.AdresseDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.example.Medical.App.utils.Constants.APP_ROOT;
+
+@Api(APP_ROOT + "/adresses")
 public interface AdresseApi {
+
+
+    @PostMapping(value = APP_ROOT + "/adresses/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Enregistrer une adresse", notes = "cette methode permet d'enregistrer ou de modifier une adresse", response = AdresseDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "L'objet adresse cree / modifier"),
+            @ApiResponse(code = 400, message = "L'objet adresse n'est pas valide")
+    })
+    AdresseDto save(@RequestBody AdresseDto dto);               //@RequestBody: c'est pour la deserialisation c-a-d la transformation d'un text du format json vers cette objet de type ArticleDto
+
+    @GetMapping(value = APP_ROOT + "/adresses/{idAdresse}", produces = MediaType.APPLICATION_JSON_VALUE)
+    AdresseDto findById(@PathVariable("idAdresse") Long id);
+
+    @GetMapping(value = APP_ROOT + "/adresses/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<AdresseDto> findAll();
+
+    @DeleteMapping(value = APP_ROOT + "/adresses/{idAdresse}")
+    void delete(@PathVariable("idAdresse") Long id);
+
+    @PutMapping(value = APP_ROOT + "/adresses/{idAdresse}/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    AdresseDto update(@PathVariable("idAdresse") Long id, @RequestBody AdresseDto dto);
 }
---
+++ src/main/java/com/example/Medical/App/controlleurs/api/AssuranceApi.java
@@ -0,0 +1,32 @@
+package com.example.Medical.App.controlleurs.api;
+
+import com.example.Medical.App.dto.AssuranceDto;
+import io.swagger.annotations.Api;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.example.Medical.App.utils.Constants.APP_ROOT;
+
+@Api(APP_ROOT + "/assurances")
+public interface AssuranceApi {
+
+    @PostMapping(value = APP_ROOT + "/assurance/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    AssuranceDto save (@RequestBody AssuranceDto assuranceDto);
+
+    @GetMapping(value = APP_ROOT + "/assurance/{idAssurance}", produces = MediaType.APPLICATION_JSON_VALUE)
+    AssuranceDto findById (@PathVariable("idAssurance") Long id);
+
+    @GetMapping(value = APP_ROOT + "/assurance/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<AssuranceDto> findAll ();
+
+    @GetMapping(value = APP_ROOT + "/assurance/{nomAssureur}", produces = MediaType.APPLICATION_JSON_VALUE)
+    AssuranceDto findByNomAssureur(@PathVariable("nomAssureur") String nomAssureur);
+
+    @PutMapping(value = APP_ROOT + "/assurance/{idAssurance}/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    AssuranceDto update (@PathVariable("idAssurance") Long id, @RequestBody AssuranceDto assuranceDto);
+
+    @DeleteMapping(value = APP_ROOT + "/assurance/delete/{idAssurance}")
+    void delete (@PathVariable("idAssurance") Long id);
+}
