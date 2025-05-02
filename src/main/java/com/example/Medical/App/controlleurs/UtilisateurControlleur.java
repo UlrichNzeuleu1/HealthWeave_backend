744ca0e59090fd006c6e8449f006ce57a722bda4
Ulrich Nzeuleu <ulrichnzeuleu@gmail.com>
2025-05-03 01:48:03 +0200

    implementation du services, du controlleur et des interfaces pour l'entite Utilisateur.java

--- src/main/java/com/example/Medical/App/controlleurs/UtilisateurControlleur.java
+++ src/main/java/com/example/Medical/App/controlleurs/UtilisateurControlleur.java
@@ -1,7 +1,50 @@
 package com.example.Medical.App.controlleurs;
 
+import com.example.Medical.App.controlleurs.api.UtilisateurApi;
+import com.example.Medical.App.dto.UtilisateurDto;
+import com.example.Medical.App.services.interfaces.UtilisateurService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class UtilisateurControlleur {
+public class UtilisateurControlleur implements UtilisateurApi {
+
+    private final UtilisateurService service;
+
+    @Autowired
+    public UtilisateurControlleur(UtilisateurService service) {
+        this.service = service;
+    }
+
+    @Override
+    public UtilisateurDto save(UtilisateurDto utilisateurDto) {
+        return service.save(utilisateurDto);
+    }
+
+    @Override
+    public UtilisateurDto findById(Long id) {
+        return service.findById(id);
+    }
+
+    @Override
+    public UtilisateurDto findByNom(String nom) {
+        return service.findByNom(nom);
+    }
+
+    @Override
+    public List<UtilisateurDto> findAll() {
+        return service.findAll();
+    }
+
+    @Override
+    public UtilisateurDto update(Long id, UtilisateurDto utilisateurDto) {
+        return service.update(id,utilisateurDto);
+    }
+
+    @Override
+    public void delete(Long id) {
+        service.delete(id);
+    }
 }
--- src/main/java/com/example/Medical/App/controlleurs/api/UtilisateurApi.java
+++ src/main/java/com/example/Medical/App/controlleurs/api/UtilisateurApi.java
@@ -1,4 +1,33 @@
 package com.example.Medical.App.controlleurs.api;
 
+import com.example.Medical.App.dto.RendezVousDto;
+import com.example.Medical.App.dto.UtilisateurDto;
+import io.swagger.annotations.Api;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.example.Medical.App.utils.Constants.APP_ROOT;
+
+@Api(APP_ROOT+ "/utilisateurs")
 public interface UtilisateurApi {
+
+    @PostMapping(value = APP_ROOT + "/utilisateurs/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    UtilisateurDto save (@RequestBody UtilisateurDto utilisateurDto);
+
+    @GetMapping(value = APP_ROOT + "/utilisateurs/{idUtilisateur}", produces = MediaType.APPLICATION_JSON_VALUE)
+    UtilisateurDto findById (@PathVariable("idUtilisateur") Long id);
+
+    @GetMapping(value = APP_ROOT + "/utilisateurs/{nomUtilisateur}", produces = MediaType.APPLICATION_JSON_VALUE)
+    UtilisateurDto findByNom (@PathVariable("nomUtilisateur") String nom);
+
+    @GetMapping(value = APP_ROOT + "/utilisateurs/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<UtilisateurDto> findAll ();
+
+    @PutMapping(value = APP_ROOT + "/utilisateurs/{idUtilisateur}/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    UtilisateurDto update (@PathVariable("idUtilisateur") Long id, @RequestBody UtilisateurDto utilisateurDto);
+
+    @DeleteMapping(value = APP_ROOT + "/utilisateurs/delete/{idUtilisateur}")
+    void delete (@PathVariable("idUtilisateur") Long id);
 }
